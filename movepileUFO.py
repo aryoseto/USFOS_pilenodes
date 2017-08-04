2d516f15a2fa4b03d9bca4b318256ce7772a2ef7
aryoseto <aryo.seto@gmail.com>
2017-08-04 03:00:15 +0100

    uprev 0.1.1, error msg added, neater column printing

--- movepileUFO.py
+++ movepileUFO.py
@@ -1,6 +1,6 @@
 __author__ = 'aryo.seto'
 __copyright__='Copyright 2017, The Lazy Project'
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __date__ = "3-Aug-2017"
 
 """
@@ -13,6 +13,7 @@ To move back the piles above the mudline to their correct location.
                 control_file.inp
                 1st line -- ufo_source_file.fem
                 2nd line -- number of nodes to be corrected [int]
+                3rd line -- correction value to be added
 
 
 Next development:
@@ -49,16 +50,74 @@ def toList(nameoffile):
     return llist
 
 
+# Function to read and write ufo file and adjusting the pile coordinates
+def readwrite (target_name, source_name, number_of_nodes, correction_value):
+    with open(target_name, 'w') as outfile:
+        try:
+            with open(source_name, 'r') as infile:
+                counter = 0
+                for line in infile:
+                    if 'NODE ' in line and counter <= (number_of_nodes - 1):
+                        # Counting how many nodes have been read
+                        counter +=1
+                        nodes = line.split()
+                        # Correct the z coordinates
+                        zcoord = float(nodes[4]) + correction_value
+                        outfile.write('{: >5} {: >14} {: >14} {: >14} {: >14}'.format(nodes[0],nodes[1],nodes[2],nodes[3],str(zcoord)) + '\n')
+                    else:
+                        outfile.write(line)
+            res = 1
+        except:
+            print('source file not found')
+            res = 0
+    return res
+
+
 # ----- Execute Program ------
+
+# Check if control file is given and valid
 resargv = checkargv()
 
 if resargv == 1:
     # echoing the name of the control file
     print('Reading control file: %s' % sys.argv[1])
-    OpenFileName = sys.argv[1]
 else:
     # spit this when the control file not found, stop the program
     sys.exit('ERROR - Oops! Control file not found')
+
+
+# Check if control file is giving the right parameter
+# Obtaining list of parameters
+ctrlpar = toList(sys.argv[1])
+
+# Check if the source file exists
+if os.path.exists(ctrlpar[0]):
+    print('ufo source file : {} '.format(ctrlpar[0]) + 'found' )
+else:
+    print('{} not found! check your control file'.format(ctrlpar[0]))
+    sys.exit('ERROR')
+
+
+# Check the number of nodes given
+if ctrlpar[1] == 0:
+    sys.exit('ERROR - number of nodes cannot be 0')
+else:
+    try:
+        int(ctrlpar[1])
+        print('number of nodes to be adjusted : {}'.format(ctrlpar[1]))
+    except:
+        sys.exit('ERROR - number of nodes cannot be {}'.format(ctrlpar[1]))
+
+
+# Check the adjustment value
+if ctrlpar[2] == 0:
+    sys.exit('ERROR - adjustment value cannot be 0')
+else:
+    try:
+        float(ctrlpar[2])
+        print('adjustment value : {}'.format(ctrlpar[2]))
+    except:
+        sys.exit('ERROR - adjustment value cannot be {}'.format(ctrlpar[2]))
 
 
 # Obtaining parameters from control file
@@ -66,8 +125,6 @@ else:
     ctrlpar[1] --> number of nodes to be modified
     ctrlpar[2] --> z coordinate adjustment
 """
-# Obtaining list of parameters
-ctrlpar = toList(sys.argv[1])
 
 # Obtaining source file name
 srcname = ctrlpar[0]
@@ -81,17 +138,10 @@ numberofnodes = int(ctrlpar[1])
 # Obtaining z correction value
 zcorrection = float(ctrlpar[2])
 
-# identify the pile node lines and correcting the z coordinate
-with open(modname, 'w') as outfile:
-    with open(srcname, 'r') as infile:
-        counter = 0
-        for line in infile:
-            if 'NODE ' in line and counter <= (numberofnodes - 1):
-                # Checking how many nodes has been read
-                counter += 1
-                nodes = line.split()
-                # Correct the z coordinates
-                zcoord = float(nodes[4]) + zcorrection
-                outfile.write(' ' + nodes[0] + '         ' + nodes[1] + '   ' + nodes[2] + '   ' + nodes[3] + '   ' + str(zcoord) + '\n')
-            else:
-                outfile.write(line)
+
+status = readwrite(modname, srcname, numberofnodes, zcorrection)
+
+if status == 1:
+    print('Normal Exit')
+else:
+    print('ERROR - produced file will not be correct')
